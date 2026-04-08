b5d22eb4785ec5f4b21f96369c9d8fabeec88ec0
Kaalig <adamrenar@gmail.com>
2026-04-08 20:41:04 +0200

    onclick buttons

--- backend/main.js
+++ backend/main.js
@@ -1,2 +1,14 @@
 const DAYS = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
 const MONTHS = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+
+
+function show_tab(tabName){
+    document.querySelector("Dashboard").style.display = "none";
+    document.querySelector("Notes").style.display = "none";
+    document.querySelector("Habits").style.display = "none";
+    document.querySelector("Rendez-vous").style.display = "none";
+    document.querySelector(tabname).style.display = "block";
+  
+}
+
+
