a68481a97efe4fc992e6a072557f6029fb01137c
Kaalig <adamrenar@gmail.com>
2026-05-05 02:36:24 +0200

    error handling .bat

--- sunset.bat
+++ sunset.bat
@@ -1,4 +1,22 @@
 @echo off
+
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python n'est pas installé !
+    echo Télécharge-le ici : https://www.python.org/downloads/
+    pause
+    exit
+)
+pip show flask >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installation de Flask pour l'initialisation de Sunset...
+    pip install flask
+     if %errorlevel% neq 0 (
+        echo Erreur : Flask n'a pas pu être installé !
+        pause
+        exit
+    )
+)
 echo Lancement de Sunset...
 start http://localhost:5000
 python app.py
